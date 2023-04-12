397d7c1b9175d63374ebf5942fbb71496fbb1797
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-04-12 09:23:48 -0400

    navbar created; need to work on links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,39 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
-    <div className='flex justify-between items center w-full h-full px-2 2xl:px-16'>
-    <Image src='/../public/assets/dino.png' alt='/' width='105' height='50'/>
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/dino.png"
+          alt="/"
+          width="105"
+          height="50"
+        />
+        <div>
+          <ul>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b"> Home</li>{" "}
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b"> About</li>{" "}
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b"> Skills</li>{" "}
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b"> Projects</li>{" "}
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b"> Contact</li>{" "}
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
