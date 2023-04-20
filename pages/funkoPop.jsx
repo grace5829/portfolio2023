498f6c97717b359bf28bfe19c5d1eedcff8e39b8
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-04-20 14:18:33 -0400

    removing funkopo

--- pages/funkoPop.jsx
+++ pages/funkoPop.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import FunkoPopTopImg from '../public/assets/projects/FunkoPopTopImg.png'
+// import FunkoPopTopImg from '../public/assets/projects/FunkoPopTopImg.png'
 import {RiRadioButtonFill} from 'react-icons/ri'
 import Link from 'next/link'
 const FunkoPop = () => {
@@ -8,7 +8,7 @@ const FunkoPop = () => {
     <div className='w-full'>
       <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80  z-10'/> 
-        <Image className='absolute  z-1' layout='fill' objectFit='cover' src={FunkoPopTopImg} alt='/' />
+        {/* <Image className='absolute  z-1' layout='fill' objectFit='cover' src={FunkoPopTopImg} alt='/' /> */}
       <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white  z-10 p-2'> 
         <h1 className='py-2'>FunkoPop Shop</h1>
       </div>
